7a5227532a8407d4ac2de4607d9cd59177d14830
Shruti-jha6 <shruti119btcsai22@igdtuw.ac.in>
2024-08-09 18:49:01 +0530

    localstorage save updated

--- script.js
+++ script.js
@@ -15,14 +15,25 @@ function addtask(){
         listcont.appendChild(span);
     }
     inputBox.value="";
+    saveData();
 }
 listcont.addEventListener("click", function(e){
     if(e.target.tagName==="LI"){
         e.target.classList.toggle("check");
+        saveData();
 
     }
     else if(e.target.tagName==="SPAN"){
         e.target.parentElement.remove();
+        // saveData();
     }
 
 },false);
+
+function saveData(){
+    localStorage.setItem("data", listcont.innerHTML);
+}
+function showData(){
+    listcont.innerHTML =localStorage.getItem("data");
+}
+showData();
