b6273579f2590b8431cc905d1b640f2e4ae05088
Shruti-jha6 <shruti119btcsai22@igdtuw.ac.in>
2024-08-09 18:39:31 +0530

    remove task function

--- script.js
+++ script.js
@@ -16,3 +16,13 @@ function addtask(){
     }
     inputBox.value="";
 }
+listcont.addEventListener("click", function(e){
+    if(e.target.tagName==="LI"){
+        e.target.classList.toggle("check");
+
+    }
+    else if(e.target.tagName==="SPAN"){
+        e.target.parentElement.remove();
+    }
+
+},false);
